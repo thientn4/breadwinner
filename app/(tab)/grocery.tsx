5841c97ecfd8f851b5595cda8c9f5704c1c6d3e5
thientn4 <ntmthien2001@gmail.com>
2025-07-29 22:32:33 -0700

    add monthly limit for free build

--- app/(tab)/grocery.tsx
+++ app/(tab)/grocery.tsx
@@ -240,9 +240,19 @@ export default function Index() {
           </TouchableOpacity>
         </View>
         <TouchableOpacity style={{...styles.buttonInput,aspectRatio:1,marginLeft:10}}  onPress={async ()=>{
+          ////////////////////////////////////////////////////////////////////////////////////////////////////
+          let now = new Date()
+          now=`${now.getFullYear()}${(now.getMonth() + 1).toString().padStart(2, '0')}`
+          let lastUse=await longTermStorage.retrieve('lastUse')
+          let freeCount=await longTermStorage.retrieve('freeCount')
+          freeCount=freeCount?parseInt(freeCount):2
+          if(lastUse===now && freeCount===0)return Alert.alert('Breadwinner','You have used all free build attempts for this month.\nGo premium for unlimited access!')
+          ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+          
           Alert.alert(
             'Breadwinner',
-            "Are you sure, you want to rebuild this grocery list?",
+            `Are you sure you want to rebuild this grocery list? You have ${freeCount} free build attempt${freeCount>1?'s':''} left!`,
             [
               {
                 text: "No",
@@ -290,6 +300,10 @@ export default function Index() {
                   groceries[groceryIndex]=groceryList
                   setGrocery(groceryList)
                   longTermStorage.store('groceries',JSON.stringify(groceries))
+                  ////////////////////////////////////////////////////////////////////////////////////////////////////
+                  longTermStorage.store('lastUse',now)
+                  longTermStorage.store('freeCount',JSON.stringify(Math.max(freeCount-1,0)))
+                  ////////////////////////////////////////////////////////////////////////////////////////////////////
                   setUpdated(true)
                 },
               },
