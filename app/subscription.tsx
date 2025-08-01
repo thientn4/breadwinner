2c4b4137ca101266c7f3b3a09bd8510927801e70
thientn4 <ntmthien2001@gmail.com>
2025-08-01 15:02:49 -0700

    redesign subscription page

--- app/subscription.tsx
+++ app/subscription.tsx
@@ -1,7 +1,9 @@
+import { useIsFocused } from "@react-navigation/native";
 import { router } from "expo-router";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Image, StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import * as longTermStorage from '../support/longTermStorage';
 
 const styles=StyleSheet.create({
   column:{
@@ -38,6 +40,16 @@ const styles=StyleSheet.create({
   }
 })
 export default function Index() {
+  const now=Date.now()
+  const [expirationDate,setExpirationDate]=useState(null)
+  useEffect(()=>{
+    let getExpriation=async ()=>{
+      let data=await longTermStorage.retrieve('expiration')
+      console.log(data,now)
+      if(data)setExpirationDate(parseInt(expiration))
+    }
+    getExpriation()
+  },[useIsFocused()])
   return (
     //ignore system bar for iOS (SafeAreaView) & android (margin & padding)
     <SafeAreaView style={{ flex: 1, backgroundColor: 'white',paddingBottom: StatusBar.currentHeight, alignItems: 'center'}}>
@@ -69,18 +81,20 @@ export default function Index() {
             padding:20,
             paddingTop:0
           }}>
-            <Text style={{fontSize:65}}>$6</Text>
-            <Text style={{fontSize:20,margin:20}}>30 days</Text>
+            {((expirationDate||0)<now) && <Text style={{fontSize:65}}>$6</Text>}
+            {((expirationDate||0)<now) && <Text style={{fontSize:20,margin:20}}>30 days</Text>}
             <View>
               <View style={{height:'50%',backgroundColor:'rgb(254, 204, 109)',width:'100%',position:'absolute',bottom:0}}/>
               <Text style={{fontWeight:'bold',fontSize:20}}>Unlimited Access</Text>
             </View>
-            {/* <Text style={{fontSize:20,margin:20}}>until</Text>
-            <Text style={{fontWeight:'bold',fontSize:20}}>02/02/2032</Text> */}
+            {((expirationDate||0)>=now) && <Text style={{fontSize:20,margin:20}}>until</Text>}
+            {((expirationDate||0)>=now) && <Text style={{fontWeight:'bold',fontSize:20}}>{expirationDate}</Text>}
           </View>
-            <TouchableOpacity style={{...styles.buttonInput,backgroundColor:'rgb(58,58,58)'}} onPress={async ()=>{
-              
-            }}><Text style={styles.boldText}>Unlock</Text></TouchableOpacity>
+          {((expirationDate||0)<now) && <TouchableOpacity style={{...styles.buttonInput,backgroundColor:'rgb(58,58,58)'}} onPress={async ()=>{
+            longTermStorage.store('expiration',`${Date.now()+1 * 24 * 60 * 60 * 1000 }`)
+            longTermStorage.remove('freeCount') //freeCount and premium must be reset at the same time
+            alert('Premium successfully unlocked for this phone!')
+          }}><Text style={styles.boldText}>Unlock</Text></TouchableOpacity>}
         </View>
       </View>
       <View style={{...styles.row,backgroundColor: 'white',paddingBottom:20}}>
